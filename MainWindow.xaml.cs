d40910b08421369bfbbe00b1ae9a4477f8f70e26
kropli <dimakirimail@gmail.com>
2024-02-20 11:50:58 +0200

    Add test method
    - added Microsoft.AspNet.WebApi.Client package to project for HttpClient
    - created test method for geting db artists
    - implemented simple UI for test method

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,9 @@
-﻿using System.Text;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media;
+using MusicApp.Service.Responses;
+using MusicApp.Service;
 
 namespace MusicApp
 {
@@ -16,9 +12,53 @@ namespace MusicApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private HttpServiceClient _client;
         public MainWindow()
         {
             InitializeComponent();
+            _client = new HttpServiceClient();
+        }
+
+        public static ImageSource ConvertBase64ToImage(string base64String)
+        {
+            byte[] byteBuffer = Convert.FromBase64String(base64String);
+
+            using (MemoryStream memoryStream = new MemoryStream(byteBuffer))
+            {
+                memoryStream.Position = 0;
+
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = memoryStream;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+
+        public class MusicItem
+        {
+            public ImageSource ImagePath { get; set; }
+            public string Name { get; set; }
+
+            public MusicItem(string base64String, string name)
+            {
+                ImagePath = ConvertBase64ToImage(base64String);
+                Name = name;
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var favArtists = (await _client.GetAsync<IEnumerable<ArtistResponse>>("artists")).ToList();
+            List<MusicItem> musicItems = new List<MusicItem>();
+            foreach (var artist in favArtists)
+            {
+                musicItems.Add(new MusicItem(artist.Base64Image, artist.Name));
+            }
+
+            lvFavorite.ItemsSource = musicItems;
         }
     }
 }
